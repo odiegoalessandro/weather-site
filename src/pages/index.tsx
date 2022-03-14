48d6d4ada7bc06b7d5b19a1738f91771cbf9d423
Diego Alessandro <diegoalessandrodacruzmartins@gmail.com>
2022-03-14 16:49:02 -0300

    chore: add nookies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
-import { Flex, Container, Heading } from "@chakra-ui/react"
+import { Flex, Container, Heading, FormControl, Input } from "@chakra-ui/react"
+import { GetServerSideProps, GetStaticProps } from "next"
+import { useEffect } from "react"
+import weather from "../lib/weather"
 
 export default function Home() {
   return (
     <Flex>
-      <Container>
-        <Heading>Hello world</Heading>
-      </Container>
+      
     </Flex>
   )
 }
